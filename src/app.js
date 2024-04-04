efbfd3f7417a7e4b9da426e38880abfb1fa403a4
yp8866 <122187543+yp8866@users.noreply.github.com>
2024-04-04 20:35:01 +0530

    made some utils

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app= express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+app.use(express.json({
+    limit: "20kb"
+}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then((res)=>{
+    app.on("error",(error)=>{
+        console.log("Express app error in index.js : ",error);
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server started on port http://localhost:${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log("DB connect error in index.js: ",err);
+})
 
 
 
