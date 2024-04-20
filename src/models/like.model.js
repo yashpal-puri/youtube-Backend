e45f362dfc2e46f6c7a1d2337d313242abc7ed09
yp8866 <122187543+yp8866@users.noreply.github.com>
2024-04-20 21:23:23 +0530

    add rest of the models

---
+++ src/models/like.model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const likeSchema = new Schema({
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: "Tweet",
+    }
+},{
+    timestamps: true,
+})
+
+export const Like = model("Like",likeSchema);
