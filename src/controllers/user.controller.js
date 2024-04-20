36430d67a3dd733eb21fc93bb0703d2528504ea7
yp8866 <122187543+yp8866@users.noreply.github.com>
2024-04-20 20:39:25 +0530

    fixed register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,64 +19,63 @@ const registerUser = asyncHandler( async (req,res) =>{
     // remove password & refresh token from response
     // check if response has arrived or not
     // return response or error based on user creation
-
     const {username,email,fullname,password}= req.body
     const avatarLocalPath = req.files?.avatar?.[0]?.path||null;
     const coverImageLocalPath = req.files?.coverImage?.[0]?.path || null;   
 
-
-    if([fullname,email,username,password].some((field)=> field?.trim()==="")){
-        avatarLocalPath?fs.unlinkSync(avatarLocalPath):null;
-        coverImageLocalPath?fs.unlinkSync(coverImageLocalPath):null;
-        throw new apiError(400,"All fields are required");
-    }
-    if(!email.includes('@') || !email.includes('.')) {
-        avatarLocalPath?fs.unlinkSync(avatarLocalPath):null;
-        coverImageLocalPath?fs.unlinkSync(coverImageLocalPath):null;
-        throw new apiError(400,"Check your email properly");
-    }
-
-
-    const existedUser =await User.findOne({
-        $or: [{ username } , { email }]
-    })
-    if(existedUser) {
-        avatarLocalPath?fs.unlinkSync(avatarLocalPath):null;
-        coverImageLocalPath?fs.unlinkSync(coverImageLocalPath):null;
-        throw new apiError(409,"User with email or username already exists")
-    }
+    try {   
+    
+        if([fullname,email,username,password].some((field)=> field?.trim()==="")){
+            throw new apiError(400,"All fields are required");
+        }
+        if(!email.includes('@') || !email.includes('.')) {
+            throw new apiError(400,"Check your email properly");
+        }
+    
+    
+        const existedUser =await User.findOne({
+            $or: [{ username } , { email }]
+        })
+        if(existedUser) {
+            throw new apiError(409,"User with email or username already exists")
+        }
+        
+        
+        
+        if(!avatarLocalPath){ 
+            throw new apiError(400,"Avatar file is mandatory");
+        }
+        const avatar= await uploadOnCloudinary(avatarLocalPath);
+        const coverImage= await uploadOnCloudinary(coverImageLocalPath);
+        if(!avatar) throw new apiError(400,"Error in uploading avatar image");
     
     
     
-    if(!avatarLocalPath){ 
-        coverImageLocalPath?fs.unlinkSync(coverImageLocalPath):null;
-        throw new apiError(400,"Avatar file is mandatory");
-    }
-    const avatar= await uploadOnCloudinary(avatarLocalPath);
-    const coverImage= await uploadOnCloudinary(coverImageLocalPath);
-    if(!avatar) throw new apiError(400,"Error in uploading avatar image");
-
-
-
-    const user = await User.create({
-        fullname,
-        avatar: avatar.url,
-        coverImage: coverImage?.url||"",
-        email,
-        password,
-        username:username.toLowerCase(),
-    })
+        const user = await User.create({
+            fullname,
+            avatar: avatar.url,
+            coverImage: coverImage?.url||"",
+            email,
+            password,
+            username:username.toLowerCase(),
+        })
+        
+        const createdUser = await User.findById(user._id).select(
+            "-password -refreshToken"
+        )
     
-    const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
-    )
-
-    if(!createdUser){
-        throw new apiError(500,"Something went wrong while registering");
+        if(!createdUser){
+            throw new apiError(500,"Something went wrong while registering");
+        }
+        res.status(201).json(
+            new apiResponse(200,createdUser,"User created successfully")
+        )
+    } catch (error) {
+        throw new apiError(400, "Error in user registration: "+error.message);
+    } finally{
+        fs.existsSync(avatarLocalPath)?fs.unlinkSync(avatarLocalPath):null;
+        fs.existsSync(coverImageLocalPath)?fs.unlinkSync(coverImageLocalPath):null;
     }
-    return res.status(201).json(
-        new apiResponse(200,createdUser,"User created successfully")
-    )
 
 })
 const generateAndSaveAccessRefreshToken = async (userId)=>{
@@ -277,7 +276,7 @@ const updateAccountDetails = asyncHandler ( async (req,res)=>{
             {
                 $set: {
                     email : newemail ? newemail : req.user.email,
-                    fullName : newfullname? newfullname : req.user.fullName
+                    fullname : newfullname? newfullname : req.user.fullName
                 }
             },
             {new : true}
@@ -364,7 +363,6 @@ const updateUserCoverImage = asyncHandler (async (req,res)=>{
 const getChannelDetails = asyncHandler( async (req,res)=>{
     try {
         const {username} = req.params;
-        console.log(username);
         if(!username?.trim())
             throw new apiError(400,'Invalid channel username');
     
